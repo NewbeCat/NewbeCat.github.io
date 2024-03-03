88fda575c38d0f05595c3660df7b57d82664d1ff
NewbeCat <catonamat098@gmail.com>
2024-03-04 01:22:06 +0900

    fuxck

--- src/images.js
+++
@@ -1,7 +0,0 @@
-const images = ["0.PNG", "1.PNG", "2.PNG"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-
-document.body.style.backgroundImage = `url(img/${chosenImage})`;
